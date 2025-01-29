80fa52d96f495446b48d97206977870f35521d92
k1arthi1 <155720361+k1arthi1@users.noreply.github.com>
2025-01-29 09:34:04 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,11 @@
+class product
+  {
+    public static void main (String aregs[])
+    {
+      int a,b,product;
+      a=9;
+      b=4;
+      product=a*b;
+      System.out.println("* of two no is "=product);
+    }
+  }
